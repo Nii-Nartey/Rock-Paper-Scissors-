05aef1b43bc0af3deaf13a38361c3bdcae6b7382
Nii Nartey <nii.tetteh-charway@amalitech.com>
2023-08-22 23:23:10 +0000

    progressOne

--- assets/scripts/apps.js
+++ assets/scripts/apps.js
@@ -1,3 +1,86 @@
-const pickUserHandler = (pickHand)=>{
+//objects to hold image values
+const rps = {
+  paper: './images/paper.png',
+  rock: './images/rock.png',
+  scissors: './images/scissors.png',
+};
 
-}
+let scores = 0;
+
+//user Selection handler
+const pickUserHandler = (pickHand) => {
+  let hand = document.querySelector('.pick');
+  hand.style.display = 'none';
+  let match = document.querySelector('.match');
+  match.style.display = 'flex';
+
+  //setting user pick
+  const userPick = document.getElementById('u-pick');
+  userPick.src = rps[pickHand];
+  let cpu = cpuPickHandler();
+  referee(pickHand, cpu);
+};
+
+//Cpu selection handler
+const cpuPickHandler = () => {
+  let picks = ['rock', 'paper', 'scissors'];
+  let cpu = picks[Math.floor(Math.random() * picks.length)];
+  const cpuPick = document.getElementById('cpu-pick');
+  cpuPick.src = rps[cpu];
+  return cpu;
+};
+
+const referee = (user, cpu) => {
+  if (user == 'paper' && cpu == 'scissors') {
+    decider('YOU LOSE');
+  }
+  if (user == 'paper' && cpu == 'rock') {
+    decider('YOU WIN');
+    setScore(scores + 1);
+  }
+  if (user == 'paper' && cpu == 'paper') {
+    decider("It's a tie!");
+  }
+  if (user == 'rock' && cpu == 'scissors') {
+    decider('YOU WIN!');
+    setScore(scores + 1);
+  }
+  if (user == 'rock' && cpu == 'paper') {
+    decider('YOU LOSE!');
+  }
+  if (user == 'rock' && cpu == 'rock') {
+    decider("It's a tie!");
+  }
+  if (user == 'scissors' && cpu == 'scissors') {
+    decider("It's a tie!");
+  }
+  if (user == 'scissors' && cpu == 'rock') {
+    decider('YOU LOSE!');
+  }
+  if (user == 'scissors' && cpu == 'paper') {
+    decider('YOU WIN!');
+    setScore(scores + 1);
+  }
+};
+const decider = (decide) => {
+  let decision = document.querySelector('.who-won');
+  decision.innerText = decide;
+  console.log(decide);
+};
+
+const setScore = (score) => {
+  scores = score;
+  const scoreBoard = document.querySelector('.score-div h1');
+  scoreBoard.innerText = score;
+  console.log(scoreBoard);
+};
+
+const resetGame = () => {
+  let hand = document.querySelector('.pick');
+  hand.style.display = 'flex';
+  let match = document.querySelector('.match');
+  match.style.display = 'none';
+};
+
+const newGame = document.querySelector('.new-game');
+newGame.addEventListener('click', resetGame);
